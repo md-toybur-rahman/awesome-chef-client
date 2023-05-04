d8cb18bf61e8bdb8caddfaa626feaa284b3771fa
md-toybur-rahman <toyburrahman735@gmail.com>
2023-05-04 14:06:39 +0600

    hide environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMGQrKL_5cSrhhIBFtLctol1tFdFvdFPE",
-  authDomain: "awesome-chef-9bf80.firebaseapp.com",
-  projectId: "awesome-chef-9bf80",
-  storageBucket: "awesome-chef-9bf80.appspot.com",
-  messagingSenderId: "398401515005",
-  appId: "1:398401515005:web:064dbd6518ed401e3dbd94"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
